88378982268907c10bf46711fd5799d4ad033a7a
Erika Kodelia <erika.kodelia@wolox.com.ar>
2020-02-12 00:33:08 -0300

    Agregar navegacion al sitio web

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
 import './scss/application.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 import SignUp from './screens/SignUp/index.js';
-// import Login from './screens/Login/Login';
+import Login from './screens/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <SignUp />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/sign_up">
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
